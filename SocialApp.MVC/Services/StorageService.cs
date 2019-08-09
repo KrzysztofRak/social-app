ac302facc68afb41eb2874ec734277c0a105a4da
tbr09 <mtyborowski09@gmail.com>
2019-08-09 09:44:19 +0200

    Azure Storage Account

---
+++ SocialApp.MVC/Services/StorageService.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using SocialApp.MVC.Contracts;
+
+namespace SocialApp.MVC.Services
+{
+    public class StorageService : IStorageService
+    {
+        private readonly CloudStorageAccount storageAccount;
+        private readonly CloudBlobClient blobClient;
+        private readonly IConfiguration configuration;
+
+        public StorageService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            storageAccount = CloudStorageAccount.Parse(configuration.GetValue<string>("ConnectionStrings:StorageConnection"));
+            blobClient = storageAccount.CreateCloudBlobClient();
+        }
+
+        public async Task UploadFile(string containerName, string filename, IFormFile file)
+        {
+            var blobContainer = blobClient.GetContainerReference(containerName);
+            await blobContainer.CreateIfNotExistsAsync();
+            
+            await blobContainer.SetPermissionsAsync(new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob
+            });
+
+            CloudBlockBlob cloudBlockBlob = blobContainer.GetBlockBlobReference(filename);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                file.CopyTo(memoryStream);
+
+                var fileToUpload = memoryStream.ToArray();
+
+                if (file != null)
+                    await cloudBlockBlob.UploadFromByteArrayAsync(fileToUpload, 0, fileToUpload.Length);
+            }
+        }
+    }
+}
